497beec97528b588db27c57de875b218008b3aba
roadg14 <douglasgc12362@gmail.com>
2022-11-08 16:50:42 -0300

    Todo dia um novo dia para aprender

---
+++ Projeto Final-Base-de-Conhecimento/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="user-admin">
+        <h1>Usuários Componente</h1>
+    </div>
+</template>
+
+<script>
+
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+
+
+export default {
+    name: 'UserAdmin',
+    data: function() {
+        return {
+            mode: "save", // Put, Post ou Delete
+            user: {},
+            users: [], // Será renderizado na tabela 
+        }
+    },
+    methods: {
+        loadUsers() { // Para carregar a lista de usuarios do backend.
+            const url = `${baseApiUrl}/users`
+            axios.get(url).then(res => {
+                this.users = res.data
+                console.log(this.users)
+            })
+        }
+    },
+    mounted() {
+        // Quando componente for carregado
+        this.loadUsers()
+    }
+}
+</script>
+
+<style>
+
+</style>
